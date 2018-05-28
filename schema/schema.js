32f9f1b101e8ca044b6d86f34c065ed8fcad3ef2
Hiroshi Imamasu <hiroshimashu@gmail.com>
2018-05-28 17:58:20 +0900

    add add function to get category

--- schema/schema.js
+++ schema/schema.js
@@ -1,15 +1,53 @@
 const graphql = require('graphql');
+const axios = require('axios');
 const {
     GraphQLObjectType,
     GraphQLString,
-    GraphQLID
+    GraphQLSchema,
+    GraphQLList,
+    GraphQLInt
 } = graphql;
 
 const UserType = new GraphQLObjectType({
     name: 'User',
-    fields: {
-        id: {type: GraphQLID}
-        //　how to define genre field??
-    }
+    fields: () =>  ({
+        id: {type: GraphQLString},
+        firstName: { type: GraphQLString },
+        age: { type: GraphQLString},
+        category: {
+            type: CategoryType,
+            resolve(parentValue, args) {
+                return axios.get(`http://localhost:3000/category/${parentValue.categoryId}`)
+                    .then(res => res.data)
+            }
+        }
+    })
+});
 
-})
+const CategoryType = new GraphQLObjectType({
+    name: "Category",
+    fields: () => ({
+        id: { type: GraphQLInt },
+        name: { type: GraphQLString }
+    })
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        user: {
+            type: UserType,
+            args: {
+                id: { type: GraphQLString }
+            },
+            resolve(parentValue, args) {
+                return axios.get(`http://localhost:3000/users/${args.id}`)
+                    .then(resp => resp.data)
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+});
